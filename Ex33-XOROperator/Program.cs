00c06f12cdd737f472291b883470aed8f530ae1e
SwapnilPadave <spadave7@gmail.com>
2022-09-19 09:59:52 +0530

    .

---
+++ Ex33-XOROperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex33_XOROperator
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            bool fno = true;
+            bool sno = false;
+            bool tno = true;
+            bool fono = false;
+
+            bool result=fno^sno^tno^fono;
+
+            Console.WriteLine("{0}^{1}^{2}^{3} ={4}", fno, sno, tno, fono, result);
+
+        }
+    }
+}
---
+++ Ex34-ForLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex34_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 0;
+            //for(int i = 0; i < 100; i--)
+            //{
+            //    x = i++;
+            //    Console.WriteLine(x);
+            //}
+
+            int x = 0;
+            for(int i = 0; i < 100; i++)
+            {
+                x += i++;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
