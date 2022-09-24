78b40cff578e1df77e7ba5544ae15376bea32eda
SwapnilPadave <spadave7@gmail.com>
2022-09-24 18:45:18 +0530

    .

--- Ex24-Classes/Program.cs
+++ Ex24-Classes/Program.cs
@@ -5,12 +5,12 @@ namespace Ex24_Classes
     #region
     //public abstract class Car
     //{
-    //    //public abstract void Moving();
+    //    public abstract void Moving();
     //    //public void Moving()
     //    //{
     //    //    Console.WriteLine("Hi");
     //    //}
-    //    //public abstract void Add(int a, int b);
+    //    public abstract void Add(int a, int b);
     //    //public void Add(int a, int b)
     //    //{
     //    //    Console.WriteLine(a + b);
@@ -19,9 +19,9 @@ namespace Ex24_Classes
     //}
     //public interface IHonda
     //{
-    //    //public void Driving();
-    //    //public void Moving();
-    //    public  void Add(int a, int b);
+    //    public void Driving();
+    //    public void Moving();
+    //    public void Add(int a, int b);
     //}
 
     //class Speed : Car, IHonda
@@ -42,17 +42,46 @@ namespace Ex24_Classes
     //        Console.WriteLine("Swapnil");
     //    }
     //}
+    //public class MyDetails
+    //{
+    //    public bool Name { get; set; }
+    //    public bool Age { get; set; }
+    //    public bool Address { get; set; }
+    //}
     //class Program
     //{
+    //    public static void GetMyDetails(Action<MyDetails> options=null)
+    //    {
+    //        var myDetails = new MyDetails();
+    //        options?.Invoke(myDetails);
+    //        if (myDetails.Name)
+    //            Console.WriteLine("My Name Is Swapnil.");
+
+    //        if (myDetails.Age)
+    //            Console.WriteLine("My Age Is 28.");
+
+    //        if (myDetails.Address)
+    //            Console.WriteLine("My Address Is Byculla.");
+    //    }
 
     //    static void Main(string[] args)
     //    {
-    //        var c = new Speed();
-    //        c.Add(5, 6);
+    //        GetMyDetails(options=> {
+    //            options.Name = true;
+    //            options.Age = true;
+    //            options.Address = true;
+    //        });
 
+    //        Console.ReadKey();
+
+            //var c = new Speed();
+            //c.Add(5, 6);
+            //c.Driving();
+    //        //c.Moving();
     //    }
     //}
     #endregion
+
 
 
 
---
+++ Ex33-LinkQquery/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex33_LinkQquery
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            string[] names = { "swapnil", "saurabh", "faisal", "ajay" };           
+
+            var Linkqquerry = from name in names
+                              where name.Contains('p')
+                              select name;
+
+            foreach(var name in Linkqquerry)
+            {
+                Console.WriteLine(name + "");
+            }           
+        }
+    }
+}
